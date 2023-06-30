e817826aa604aa06812538e26064c82813a7344a
Milinda Ranawaka <49589047+MilindaRanawaka@users.noreply.github.com>
2023-06-30 11:58:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -145,6 +145,45 @@ app.get('/test4/:id/:cd', async (req, res, next) => {
   }
 });
 
+/* Get a specific employee */
+app.get('/test5/:id/:cd', async (req, res, next) => {
+  try {
+    const { id, cd } = req.params;
+/*     const employee = await employees.findOne({
+      _id: id,
+    });
+
+    if (!employee) {
+      const error = new Error('Employee does not exist');
+      return next(error);
+    } */
+
+    res.json({
+    id: id,
+	organisasjonsnummer: cd,
+	firmanavn: 'SPORVEIEN AS',
+	postadresse: {
+		adresse: [
+			'Postboks 2857 , TØYEN'
+		],
+		postnummer: 0608,
+		poststed: 'OSLO'
+	},
+	forretningsadresse: {
+		adresse: [
+			'Økernveien 9'
+		],
+		postnummer: 0653,
+		poststed: 'OSLO'
+	},
+	konkurs: false,
+	avvikles: false
+});
+  } catch (error) {
+    next(error);
+  }
+});
+
 
 // Initialize server
 app.listen(5000, () => {
